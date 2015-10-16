8aa1363a9fbfcd7a8d92baadaf3ce506d1620780
bjglasbe <bjglasbe@scslt154.cs.uwaterloo.ca>
2015-10-16 17:04:46 -0400

    Add network drawing and dumping features

--- src/main/java/com/glasbergen/neat/examples/connectfour/ConnectFour.java
+++ src/main/java/com/glasbergen/neat/examples/connectfour/ConnectFour.java
@@ -1,22 +1,58 @@
 package com.glasbergen.neat.examples.connectfour;
 
+import java.awt.Dimension;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
 import com.glasbergen.neat.NeatParameters;
 import com.glasbergen.neat.NeuralNetwork;
+import com.glasbergen.neat.Node;
+
+import edu.uci.ics.jung.algorithms.layout.DAGLayout;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 
 public class ConnectFour {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		NeatParameters.POPULATION_SIZE = 150;
 		NeatParameters.MUTATION_POWER = 10;
 		NeatParameters.UNBRED_CONTENDER_RATE = 1.0;
 		NeatParameters.NEW_LINK_RATE = 0.3;
 		NeatParameters.NEW_NODE_RATE = 0.5;
-		NeatParameters.MAX_GENERATIONS = 100;
+		NeatParameters.MAX_GENERATIONS = 200;
 		ConnectFourOptimizer opt = new ConnectFourOptimizer();
 		NeuralNetwork net = opt.runAllGenerations(200);
 		Board b = new Board();
 		ConnectFourFitnessEvaluator.playGame(net, net, b);
 		b.dumpAsciiBoard();
-		
+		DirectedSparseGraph<Integer,Double> g = new DirectedSparseGraph<>();
+		int highestId = 0;
+		for(Node n : net.getNetworkNodes()){
+			g.addVertex(n.getId());
+			if( n.getId() > highestId ){
+				highestId = n.getId();
+			}
+			for( Node dep : n.getAllDependencies().keySet()){
+				g.addEdge(n.getAllDependencies().get(dep), dep.getId(), n.getId());
+			}
+		}
+		highestId = highestId+1;
+		for(Node n : net.getOutputNodes()){
+			g.addVertex(n.getId()+highestId);
+			for( Node dep : n.getAllDependencies().keySet()){
+				g.addEdge(n.getAllDependencies().get(dep), dep.getId(), n.getId()+highestId);
+			}
+		}
+		DAGLayout<Integer, Double> layout = new DAGLayout<>(g);
+		layout.setSize(new Dimension(1920,1080));
+		BasicVisualizationServer<Integer, Double> bvs = new BasicVisualizationServer<>(layout);
+		bvs.setPreferredSize(new Dimension(1920,1080));
+		JFrame frame = new JFrame("Neural Network");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(bvs);
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
--- src/main/java/com/glasbergen/neat/examples/connectfour/ConnectFourOptimizer.java
+++ src/main/java/com/glasbergen/neat/examples/connectfour/ConnectFourOptimizer.java
@@ -1,11 +1,29 @@
 package com.glasbergen.neat.examples.connectfour;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import com.glasbergen.neat.EvolutionOptimizer;
+import com.glasbergen.neat.NeuralNetwork;
+import com.glasbergen.neat.io.NetworkDeserializer;
+import com.glasbergen.neat.io.NetworkSerializer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class ConnectFourOptimizer extends EvolutionOptimizer {
-
-	public ConnectFourOptimizer() {
+	private Gson gson;
+	private File outputFile;
+	private PrintWriter pw;
+	public ConnectFourOptimizer() throws IOException {
 		initializeNetworks(6*7*3+1, 6);
+		gson = new GsonBuilder().registerTypeAdapter(NeuralNetwork.class, new NetworkSerializer()).registerTypeAdapter(NeuralNetwork.class, new NetworkDeserializer()).create();
+		outputFile = new File("connectFourNetworks.ser");
+		if( !outputFile.exists() ){
+			outputFile.createNewFile();
+		}
+		pw = new PrintWriter(outputFile);
+				
 	}
 	@Override
 	public void runCurrentGeneration() {
@@ -15,7 +33,10 @@ public class ConnectFourOptimizer extends EvolutionOptimizer {
 		//On IllegalArgument, the machine loses. Pick at random to give the
 		//other machine something to play against
 		ConnectFourFitnessEvaluator eval = new ConnectFourFitnessEvaluator(networks);
-		eval.rankAllNetworks();
+		networks = eval.rankAllNetworks();
+		
+		String json = gson.toJson(networks.get(0));
+		pw.println(json);
 	}
 
 }
